de9afb46f32ae8e9540bc1fc79fe4e49b59fa765
Yeram <63442636+Yeram522@users.noreply.github.com>
2021-07-05 17:46:19 +0900

    Pixel생성/소멸 스크립트 업로드
    혹시라도 충돌 있을까봐, 따로 제 이름으로 된 브랜치 만들어서 올려뒀습니다.
    다른 분들도 공유 할만한 스크립트 있으시면 브랜치 따로 파신 후에 올리시는 걸 추천드려요. 나중에 서로 상의 통해서 메인 브랜치에 합치면 됩니다!
    
    📍 ** PixelpieceController.cs **
    //<기능>
    //1. 플레이어와 충돌시 이미지 사라지고 데이터도 소멸(0)
    //2. 충돌 후 플레이어에게 메세지를 보낸다. 픽셀개수 증가하라고.(0)
    //3. 생성된지 특정시간이 되었는데도 충돌이 없다면 자동 소멸한다.(0)
    
    이 스크립트 같은 경우에는 "플레이어" 스크립트 작업하시는 분과 협업이 필요해서, 읽어보시면 이 스크립트에서 플레이어 스크립트의 GetPixel()(이름은 제가 맘대로 지은거에요)이라는 함수를 사용해, 픽셀개수 정보를 전달하면 플레이어스크립트 측에서 해당 개수 만큼 보유픽셀값을 증가시키시면 됩니다. 나중에 합칠때 스크립트 제목은 바꿀예정입니당.
    
    📍 ** SponPixelManager **
    // 픽셀이 스폰되는 오브젝트에 붙는 스크립트이다.
    // 1. 플레이어와 충돌 && 플레이어가 채집 상태일때 아이템이 젠된다.
    // 2. 아이템은 특정 확률로 젠된다. pixelspon 메서드가 수행. 기본적으로 자기의 성질값에따라 스폰하는 픽셀조각이 다르다.
    // 3. 젠은 플레이어 위치값을 받아와서 갯수만큼 주변에 뿌림
    
    📍 ** PlayerMove.cs **
    제가 디버깅할때 필요해서 쓴거라 삭제할 예정입니다.
    
    💡 아직 "SponPixelManager.cs"는 오류가 있습니다.
    💡 "PixelpieceController.cs"는 게임오브젝트에 CircleCollider2D 넣으시고 IsTrigger체크 하신 후에 스크립트 넣어서 사용하실 수 있습니다!

---
+++ dotbreeder/Assets/Scripts/PixelpieceController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//픽셀조각에 대한 클래스이다. 픽셀조각 스프라이트에 들어가고, 프리팹으로 에셋에 저장되어있다.
+//<기능>
+//1. 플레이어와 충돌시 이미지 사라지고 데이터도 소멸(0)
+//2. 충돌 후 플레이어에게 메세지를 보낸다. 픽셀개수 증가하라고.(0)
+//3. 생성된지 특정시간이 되었는데도 충돌이 없다면 자동 소멸한다.(0)
+public class PixelpieceController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //생성된 직후부터 카운트 시작. 일단은 7초뒤에 플레이어가 조각을 먹지 않으면 소멸.
+        Destroy(gameObject, 7f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player") //태그가 플레이어인지 확인
+        {
+            //PlayerMove는 플레이어 움직임 스크립트 입니다.임의로 설정해둔거라 playercontroller로 이름 바꿀게요!
+            PlayerMove playerController = other.GetComponent<PlayerMove>();
+
+            //상대방으로부터  PlayerController컴포넌트를 가져오는데 성공했다면
+            if (playerController != null) 
+            {
+                //playerController.GetPixel(GameObject); 메서드 실행: 플레이어에게 획득한 픽셀의 정보를 보낸다.
+                //Debug.Log("획득한 픽셀->플레이어 정보 전달 완료");
+                Debug.Log("획득한 픽셀->플레이어 정보 전달 요청");
+                Destroy(gameObject);//자기 자신 삭제!
+            }
+        }
+        else
+            Debug.Log("not collision with pixel");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

5f5b02dddf70f99631170f8c0b76cc21f0abde16
madongchan <mdc0078@gmail.com>
2021-08-20 01:08:36 +0900

    플레이어 이름 입력 fadeout 적용
    1. 이름 입력을 못 받았을 때
    2. 글자수가 12자 이상일 때

--- dotbreeder/Assets/Prefabs/PlayerNameIuputPrefabs/Script/NameInputField.cs
+++ dotbreeder/Assets/Prefabs/PlayerNameIuputPrefabs/Script/NameInputField.cs
@@ -10,11 +10,12 @@ public class NameInputField : MonoBehaviour
     private GameManager thePlayer;
     public TextMeshProUGUI _playerName;
     public TMP_InputField inputField;
+    public TextMeshProUGUI errorMasage;
 
     void Start()
     {
         thePlayer = FindObjectOfType<GameManager>();
-        inputField.characterLimit = 12;
+        inputField.characterLimit = 20;
         inputField.onValueChanged.AddListener(
             (word) => inputField.text = Regex.Replace(word, @"[^0-9a-zA-Z가-힣]", "")
         );
@@ -22,7 +23,31 @@ public class NameInputField : MonoBehaviour
     public void InputPlayerName()
     {
         //확인버튼 눌렀을때
-        thePlayer.characterName = _playerName.text;
-        Debug.Log(thePlayer.characterName);
+        if (inputField.text.Length == 0)
+        {
+            StartCoroutine(FadeOutToTextAlpha(1f, errorMasage.GetComponent<TextMeshProUGUI>()));
+            errorMasage.text = "이름을 입력해주세요";
+        }
+        else if (inputField.text.Length >= 12)
+        {
+            StartCoroutine(FadeOutToTextAlpha(1f, errorMasage.GetComponent<TextMeshProUGUI>()));
+            errorMasage.text = "글자가 너무 많습니다";
+        }
+        else
+        {
+            thePlayer.characterName = _playerName.text;
+            Debug.Log(thePlayer.characterName);
+        }
+    }
+
+    public IEnumerator FadeOutToTextAlpha(float t, TextMeshProUGUI i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 1);
+        i.color = Color.red;
+        while (i.color.a > 0.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t));
+            yield return null;
+        }
     }
 }
